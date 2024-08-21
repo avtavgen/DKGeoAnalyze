b5d024a2c865ed04dc1f4328a84399d17735294d
alexey.tavgen <tavgen.alexey@gmail.com>
2024-08-21 10:05:44 +0200

    add migration

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
-from db import models
+from db.models import DBTask, CSVData
 
 
 from alembic import context
@@ -15,7 +15,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url')
+config.set_main_option('sqlalchemy.url', os.environ.get("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
