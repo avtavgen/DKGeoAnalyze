ca9119c205016c57020c99eb2fa9e38384b7cca7
alexey.tavgen <tavgen.alexey@gmail.com>
2024-08-21 09:57:27 +0200

    fix merge

--- db/core.py
+++ db/core.py
@@ -1,6 +1,8 @@
 import os
+from asyncio import current_task
+from contextlib import asynccontextmanager
 
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -17,3 +19,16 @@ async def get_session() -> AsyncSession:
     async_session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session
+
+
+@asynccontextmanager
+async def scoped_session():
+    scoped_factory = async_scoped_session(
+        session_factory=sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False),
+        scopefunc=current_task
+    )
+    try:
+        async with scoped_factory() as s:
+            yield s
+    finally:
+        await scoped_factory.remove()
